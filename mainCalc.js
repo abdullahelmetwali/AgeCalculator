6a8543afa534dbe4452c7267ab4037617f68ee6b
Abdullah ElMetwali <133114413+abdullahelmetwali@users.noreply.github.com>
2023-11-29 16:51:57 +0200

    Update mainCalc.js

--- mainCalc.js
+++ mainCalc.js
@@ -6,8 +6,8 @@ let monthsOP = document.querySelector(".months")
 let yearsOP = document.querySelector(".years")
 let imgSubmit = document.querySelector(".img")
 let input = document.querySelector("input");
-let imgP = document.querySelector("img")
-imgP.src = "images/icon-arrow.svg"
+// let imgP = document.querySelector("img")
+// imgP.src = "images/icon-arrow.svg"
 
 imgSubmit.onclick = function(){
 if(daysInput.value === "" ||
